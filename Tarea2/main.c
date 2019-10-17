4f24f91e84e08c937b69d6148df58ac62ee01d5d
Tekanx <45672146+Tekanx@users.noreply.github.com>
2019-10-17 12:30:02 -0300

    Last Update casos 3 y 4

--- Tarea2/main.c
+++ Tarea2/main.c
@@ -32,9 +32,9 @@ typedef struct{
 
 void importar(char linea[1024], HashTable artista, HashTable album, HashTable cancion);
 
-Cancion *getLinea(char linea[1024], HashTable artistaHT);
+Cancion *getLinea(char linea[1024]);
 
-bool cargarArchivo(char nombre[301], HashTable artistaHT, HashTable albumHT, HashTable cancionHT);
+bool cargarArchivo(char nombre[301], HashTable *artistaHT, HashTable *albumHT, HashTable *cancionHT);
 
 int main()
 {
@@ -65,19 +65,19 @@ int main()
         case 1:
             printf("\nIngrese el nombre del archivo sin su extension :");
             scanf("%s", &nombre);
-            //Si
             strcat(nombre, ".csv");
             if(cargarArchivo(nombre, artistaHT, albumHT, cancionHT) == true) printf("Musica cargada ! \n");
             else printf("El archivo .csv no existe...\n");
             break;
         case 2:
-            printf("Se importará todas las canciones actuales a un csv nuevo !\n");
+            printf("Se importara todas las canciones actuales a un csv nuevo !\n");
             printf("ingrese nombre del archivo nuevo: ");
             scanf("%s", &linea);
             importar(linea, artistaHT, albumHT, cancionHT);
             break;
         case 3:
-            printf("Ingrese el nombre del Album que quiera ingresar: ");
+            printf("Se ingresara el album solo si no existe, caso opuesto no se creara \n");
+            printf("Ingrese nombre del Album : ");
             fgets(linea, 1023, stdin);
             strtok(linea, "\n");
             if(searchHashTable(albumHT, linea) == NULL){
@@ -97,7 +97,6 @@ int main()
                 }
             } 
             else printf("El nombre del Album que ingreso ya existe... \n"); 
-
             break;
         case 4:
             printf("Ingrese la cancion con el siguiente formato...\n");
@@ -105,10 +104,11 @@ int main()
             fgets(linea, 1023, stdin);
             strtok(linea, "\n");
             cancionAux = getLinea(linea);
+
             /**
              * Buscar primero si el artista existe, si no existe crearlo
              * Buscar segundo si el album existe, si no existe crearlo
-             * Buscar si en el Album la cancion ya existe, sino no agregarla
+             * Buscar si en el Album la cancion ya existe, sino nox agregarla
              * */
             break;
         case 5:
@@ -131,8 +131,9 @@ int main()
 
 
 void importar(char linea[1024], HashTable *artista, HashTable *album, HashTable *cancion){
+    FILE *archivo;
+    Cancion *aux = calloc(1, sizeof(Cancion));
     linea = strcat(linea,".csv");
-    FILE *archivo;  //open
     archivo = fopen(linea, "w");
     if(archivo != NULL){
          printf("EL ARCHIVO YA CONTIENE MUSICA! \n");
@@ -141,29 +142,23 @@ void importar(char linea[1024], HashTable *artista, HashTable *album, HashTable 
     }
     else{
         fprintf(archivo, "Nombre,Artista,Minutos:Segundos,Album \r");
-        Cancion *aux = firstHashTable(cancion);
+        aux = firstHashTable(cancion);
         while(aux != NULL){
-            fprintf(archivo, aux ->nombreArtista, aux -> nombreArtista, aux ->duracion, aux ->nombreAlbum);
+            fprintf(archivo, "%s,%s,%s,%s \n", aux ->nombreCancion, aux ->nombreArtista, aux ->duracion, aux ->nombreAlbum);
             aux = nextHashTable(cancion);
         }
-        fclose(archivo);
         printf("Canciones guardadas en (''%s'')! \n", linea);
     }
-} 
+    fclose(archivo);
+}
 
 Cancion *getLinea(char linea[1024]){
-    Cancion *aux = (Cancion *) calloc(1, sizeof(Cancion)); 
-    /* 
-    char cancion[101];
-    char artista[101];
-    char duracion[10];
-    char album[201];
-    */
-    strcpy(aux -> nombreCancion, strtok(linea, ","));
-    strcpy(aux -> nombreArtista, strtok(NULL, ","));
-    strcpy(aux -> duracion, strtok(NULL, ","));
-    strcpy(aux -> nombreAlbum, strtok(NULL, "\n"));
-    return aux;
+    Cancion *new = (Cancion *) calloc(1, sizeof(Cancion));
+    strcpy(new -> nombreCancion, strtok(linea, ","));
+    strcpy(new -> nombreArtista, strtok(NULL, ","));
+    strcpy(new -> duracion, strtok(NULL, ","));
+    strcpy(new -> nombreAlbum, strtok(NULL, "\n"));
+    return new;
 }
 
 bool cargarArchivo(char nombre[301], HashTable *artistaHT, HashTable *albumHT, HashTable *cancionHT){
